c5f54ca6f4aa9a1f36b64f309571e2bac018619b
arthur revelski <arevelski@managedmethods.com>
2024-10-15 17:34:48 -0600

    updated site tilte

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Arthur Revelski';
+export const SITE_TITLE = 'Arthur Revelski | Exploring Code & Beyond';
 export const SITE_DESCRIPTION = 'Welcome to Arthur Revelski\'s personal website, where I showcase my passion for web development and software engineering. Explore my latest projects, dive into insightful tech blogs, view my resume, or get in touch';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = false
