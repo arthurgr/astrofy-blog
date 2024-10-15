dacd1ff206d1fb27b1459322750a77b1204a132f
arthur revelski <arevelski@managedmethods.com>
2024-10-15 09:31:58 -0600

    updated config so theme selector selection will persist across page renders

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@
 export const SITE_TITLE = 'Arthur | Personal Portfolio Website';
 export const SITE_DESCRIPTION = 'Welcome to Arthur Revelski\'s personal website, where I showcase my passion for web development and software engineering. Explore my latest projects, dive into insightful tech blogs, view my resume, or get in touch';
 export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const TRANSITION_API = false
