b68a26d757a272a0aec657367dcab1cc72306998
Arthur Revelski <arevelski@gmail.com>
2026-07-07 21:45:54 -0600

    update projects to single link on index page and link to market margin

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,44 +3,27 @@ import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
 const schema = z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
-});
-
-export const projectSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
-    project: z.string(),
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.string().optional(),
+  heroImage: z.string().optional(),
+  badge: z.string().optional(),
+  tags: z
+    .array(z.string())
+    .refine((items) => new Set(items).size === items.length, {
+      message: "tags must be unique",
+    })
+    .optional(),
 });
 
 export type BlogSchema = z.infer<typeof schema>;
-export type ProjectSchema = z.infer<typeof projectSchema>;
 
 const blogCollection = defineCollection({
-    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
-    schema: schema,
-});
-
-const projectCollection = defineCollection({
-    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects" }),
-    schema: projectSchema,
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
+  schema: schema,
 });
 
 export const collections = {
-    'blog': blogCollection,
-    'projects': projectCollection,
-}
+  blog: blogCollection,
+};
